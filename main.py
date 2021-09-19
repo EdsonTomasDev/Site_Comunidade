f2ac2d03a935fd67f4f7a1de8f24903efb8e5002
Tomas Edson <t.edson.almeida@hotmail.com>
2021-09-19 17:39:48 -0300

    Feat: create login.html

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
+
+lista_usuarios = ['Lira', 'João', 'Alon', 'Alessandra', 'Amanda']
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
+
 @app.route("/contato")
 def contato():
     return render_template("contato.html")
+
+
+@app.route('/usuarios')
+def usuarios():
+    return render_template('usuarios.html', lista_usuarios=lista_usuarios)
+
+
+@app.route('/login')
+def login():
+    return  render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
