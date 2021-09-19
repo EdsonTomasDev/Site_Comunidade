a3ced641f40fd90ee1187273500b28d5e01b68de
Tomas Edson <t.edson.almeida@hotmail.com>
2021-09-19 20:55:45 -0300

    Feat: class forms to login and new login

---
+++ forms.py
@@ -0,0 +1,18 @@
+#Biblioteca que formata formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao = PasswordField('Confirmação da senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_criarconta = SubmitField('Criar Conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField('Criar Conta')
