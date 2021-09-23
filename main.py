5fab9f20753e78dc2986f2b75e9599e96e9201d1
Tomas Edson <t.edson.almeida@hotmail.com>
2021-09-22 21:00:24 -0300

    Feat: msg alert to login and create new login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, flash, redirect
 from forms import FormCriarConta, FormLogin
 
 app = Flask(__name__)
@@ -26,7 +26,16 @@ def usuarios():
 def login():
     form_login = FormLogin()
     form_criar_conta = FormCriarConta()
-    return  render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta )
+
+    if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
+        flash(f'Login realizado com sucesso no e-mail {form_login.email.data}!', 'alert-success')
+        return redirect(url_for('home'))
+
+    if form_criar_conta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        flash(f'Conta criada com sucesso com o e-mail {form_criar_conta.email.data}!', 'alert-success')
+        return redirect(url_for('home'))
+
+    return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta )
 
 if __name__ == '__main__':
     app.run(debug=True)
