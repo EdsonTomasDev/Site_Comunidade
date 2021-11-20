d2e060f62de608950a3304339030fcc97ce07ca2
Tomas Edson <t.edson.almeida@hotmail.com>
2021-11-20 17:09:50 -0300

    Feat: creat pip freeze requirementsgit

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
-#PAREI NO VIDEO 53. Subindo nosso site no Servidor HerokuEM 00:00 MINUTOS.
+#PAREI NO VIDEO 53. Subindo nosso site no Servidor HerokuEM 00:00 MINUTOS !.git
